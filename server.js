e9d12d952dabfbd2d28c2312bc8084adfd76a39b
tuurGevers <tuur.gevers@hotmail.com>
2022-08-14 17:40:50 +0200

    cors

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const multer = require('multer')
 const upload = multer({dest: 'images/'})
 
 
-app.use(cors({
-    origin: 'http://127.0.0.1:3000',
-    credentials: true,
-    optionSuccessStatus: 200
-}))
+app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
