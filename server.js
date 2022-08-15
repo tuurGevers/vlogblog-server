0a2a8e20db8de2836e80b7fe2e23b0855d108778
tuurGevers <tuur.gevers@hotmail.com>
2022-08-15 14:07:27 +0200

    cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const upload = multer({dest: 'images/'})
 
 
 app.use(cors({
-    origin: 'https://bnbtravelers.netlify.app/'
+    origin: 'https://bnbtravelers.netlify.app'
 }));
 
 app.use(express.json())
